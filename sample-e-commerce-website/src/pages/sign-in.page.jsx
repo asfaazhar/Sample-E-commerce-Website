307c0cf8c17585d001a8aa526f430ad94eabb7a9
asfaazhar <157738967+asfaazhar@users.noreply.github.com>
2025-03-05 03:57:24 +0530

    Delete sample-e-commerce-website directory
    deleting old tested files

--- sample-e-commerce-website/src/pages/sign-in.page.jsx
+++
@@ -1,8 +0,0 @@
-import { SignIn } from "@clerk/clerk-react";
-
-export default function SignInPage() {
-
-  return (<main className="flex items-center justify-center px-4 min-h-screen">
-    <SignIn/>
-  </main>)
-}
--- sample-e-commerce-website/src/pages/sign-up.page.jsx
+++
@@ -1,8 +0,0 @@
-import { SignUp } from "@clerk/clerk-react";
-
-export default function SignUpPage() {
-
-  return (<main className="flex items-center justify-center px-4 min-h-screen">
-    <SignUp/>
-  </main>)
-}
